f4a61f2d0b296a1e8b277c3c3ec35cd258cefec0
unknown <vadimka.kostruba@gmail.com>
2023-11-08 13:49:23 +0100

    one

---
+++ 2/assets/app.js
@@ -0,0 +1,56 @@
+//alert("prvy alert Vadim kostruba");
+
+//let prvaPremenna = 3;
+//const druhaPremenna = 7;
+
+//prvaPremenna = 20;
+
+//const meno = "Vadim kostruba";
+
+//let vysledokScitania = prvaPremenna + druhaPremenna;
+
+//vysledok(vysledokScitania);
+
+//let prvaPremenna = 9;
+//let druhaPremenna = 7;
+//let vysledokScitania = prvaPremenna + druhaPremenna;
+//vysledok(vysledokScitania);
+//alert(vysledokScitania);
+//alert();
+//alert("9 + 7");
+//alert(9 + 7);
+//alert();
+//alert(prvaPremenna + druhaPremenna);
+
+//let x = 15;
+//let y = 3;
+//let z = 4;
+//let u = 8 / 2;
+//let vysledok01 = x + y * (z - u);
+//alert(vysledok01);
+
+//let x1 = 150.2 / 8;
+//let y1 = 18.3;
+//let z1 = 20.1;
+//let u1 = 3;
+//let vysledok02 = (x1 * (y1 - z1)) / u1;
+//alert(vysledok02);
+
+//let x2 = 3 ** 2;
+//let y2 = 8;
+//let z2 = 31 / 8;
+//let vysledok03 = x2 + y2 - z2;
+//alert(vysledok03);
+
+//et mojVek = 17;
+let meno = vadim;
+let priezvisko = kostruba;
+let mojeMeno = meno + "" + priezvisko;
+
+//alert(mojeMeno);
+
+let ulica = "Namestie sv. Martina 5, ";
+let mesto = "Holic, ";
+let psc = "908 51 ";
+let adresa = ulica + mesto + psc;
+alert(adresa);
